f16605dc6dd1c82be889fba1adb7b91052d361ae
TanishT <38384138+TanishT@users.noreply.github.com>
2021-03-06 13:38:54 -0500

    day 1 prep

---
+++ Day1.java
@@ -0,0 +1,24 @@
+/*
+PLAN
+1. variables
+2. printing to console
+3. strings formatting "\n"
+4. if-else
+5. for loops
+6. while loops
+7. scanner user input 
+8. external functions 
+
+Problems:
+1. factorial calculator with while loops
+2. Coding Bat
+	a. https://codingbat.com/prob/p187868
+	b. https://codingbat.com/prob/p178986
+	c. https://codingbat.com/prob/p154485
+*/
+
+class Day1 {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
