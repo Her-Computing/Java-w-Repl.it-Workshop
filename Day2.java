e5d27154e8b7a6e0fce892f3ced52c84bf8340a6
TanishT <38384138+TanishT@users.noreply.github.com>
2021-03-13 15:31:49 -0500

    added code from day 2

--- Day2.java
+++ Day2.java
@@ -12,8 +12,53 @@ Problems:
 	c. https://codingbat.com/prob/p154485
 */
 
-class Day2 {
+// import java.io.*;
+// import java.util.*;
+
+import java.util.Scanner;
+
+class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // Scanner in = new Scanner(new File("practice_input.in"));
+    // PrintWriter out = new PrintWriter(new File("practice_input.out"));
+
+	// while loops
+	for (int i = 0; i < 5; i++) {
+		System.out.println(i);
+	}
+	System.out.println("\n");
+	int i = 0;
+	while (i < 5) {
+		System.out.println(i);
+	}
+
+	boolean run = true;
+	int i = 0;
+	while (run) {
+		System.out.print(i + " ");
+		i++;
+		if (i > 10) {
+			run = false;
+		}
+	} 
+
+	// taking user input
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter a string");
+	String str = input.nextLine();
+	System.out.println("Your sentence is: " + str);
+
+
+	//external functions
+	int x = 5;
+	int n = number(x);
+	System.out.println(n);
   }
+
+  public static int number(int i) {
+	if (i < 5) {
+		return 655;
+	}
+	return 677;
+  } 
 }
