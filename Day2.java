a70cfab2b19ac64bc3871a9bd0146d95e9469404
TanishT <38384138+TanishT@users.noreply.github.com>
2021-03-13 13:35:22 -0500

    added plan for today

--- Day2.java
+++ Day2.java
@@ -1,2 +1,18 @@
-public class Day2 {
+/*
+1. while loops
+2. scanner user input 
+3. external functions 
+
+Problems:
+1. factorial calculator with while loops
+2. Coding Bat
+	a. https://codingbat.com/prob/p187868
+	b. https://codingbat.com/prob/p178986
+	c. https://codingbat.com/prob/p154485
+*/
+
+class Day2 {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
 }
