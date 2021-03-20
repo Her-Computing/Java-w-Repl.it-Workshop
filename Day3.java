67124809ebac4c2d581dcf89f76ec7d23333664b
TanishT <38384138+TanishT@users.noreply.github.com>
2021-03-20 13:22:28 -0400

    added fileio template

---
+++ Day3.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+class Day3 {
+  public static void main(String[] args) throws IOException {
+    Scanner in = new Scanner(new File("practice_input.in"));
+    PrintWriter out = new PrintWriter(new File("practice_input.out"));
+  }
+}
