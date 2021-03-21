05632cd74e79048be3a2df473e5b16d315dcba44
TanishT <38384138+TanishT@users.noreply.github.com>
2021-03-21 15:37:12 -0400

    Update Day3.java

--- Day3.java
+++ Day3.java
@@ -5,5 +5,23 @@ class Day3 {
   public static void main(String[] args) throws IOException {
     Scanner in = new Scanner(new File("practice_input.in"));
     PrintWriter out = new PrintWriter(new File("practice_input.out"));
+	
+    int x = in.nextInt();
+    System.out.println(x);
+
+    int[] array = new int[x];
+    for (int i = 0; i < x; i++) {
+      int num = in.nextInt();
+      array[i] = num;
+    }
+
+    for (int i = 0; i < x; i++) {
+      System.out.println(array[i]);
+    }
+
+    String line = in.nextLine();
+
+    out.println(line);	
+    out.close();
   }
 }
